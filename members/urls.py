316b48c73fdb25c99ab300323c7c8c81a7bd9029
Rohit Singh <rohit11july2057@gmail.com>
2026-01-17 23:14:43 +0530

    Added serializer for Task Model

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import task_list
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('tasks/', task_list),
 ]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+def members(request):
+    return HttpResponse("Members route works!")
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+    if request.method == 'GET':
+        tasks = Task.objects.filter(user=request.user)
+        return Response(TaskSerializer(tasks, many=True).data)
+
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
