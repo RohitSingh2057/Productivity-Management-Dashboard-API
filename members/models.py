6f1a58237f79edd74a409c6090ce0f5f38f8ca4f
Rohit Singh <rohit11july2057@gmail.com>
2026-01-17 11:36:14 +0530

    Created Tasks Schema

--- members/models.py
+++ members/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import user
 
-# Create your models here.
+class tasks(models.Model):
+    user = models.ForeignKey(user, related_name='tasks')
+    task = models.CharField(max_length=35)
+    desc = models.TextField()
+    priority_choice = [('H','High'),('M','Medium'),('L','Low')]
+    priority = models.CharField(max_length=1,choices=priority_choice)
+    status_choices = [('P','Pending'),('I','In-Progress'),('C','Completed')]
+    status = models.CharField(max_length=1,choices=status_choices)
+    created_t = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True,blank=True)
+    update_t = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
